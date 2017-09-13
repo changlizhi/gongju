5c37afe41cabbf7dfc96cfd89ddc94add506171b
changlz <changlizhi@aliyun.com>
2017-09-13 14:24:04 +0800

    控制器参数设置

--- Mywriter.go
+++ Mywriter.go
@@ -2,6 +2,8 @@ package gongju
 
 import (
 	"net/http"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 type Mywriter struct {
@@ -15,3 +17,12 @@ func (w *Mywriter) Write([]byte) (int, error) {
 	return 1, nil
 }
 func (w *Mywriter) WriteHeader(par int) {}
+
+func Kongzhiqi(c *beego.Controller) {
+	c.Data = make(map[interface{}]interface{})
+	c.Ctx = context.NewContext()
+	c.Ctx.Input = context.NewInput()
+	c.Ctx.Output = context.NewOutput()
+	c.Ctx.Output.Context = context.NewContext()
+	c.Ctx.Output.Context.ResponseWriter = &context.Response{new(Mywriter), true, 200}
+}
