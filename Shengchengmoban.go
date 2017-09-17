cf277c58263962d39cbb8225de0973f5a5b0cd6f
changlz <changlizhi@aliyun.com>
2017-09-17 13:39:30 +0800

    完善，后期自动生成

--- Shengchengmoban.go
+++ Shengchengmoban.go
@@ -7,6 +7,17 @@ import (
 	"log"
 )
 
+func importsmoban(mkv string, buffer *bytes.Buffer) {
+	// \n import (\n
+	importstr := zfzhi.Zhi.Hhf() + zf.Zfs.Import(true) + zfzhi.Zhi.Kgf() +
+		zfzhi.Zhi.Xkhz() + zfzhi.Zhi.Hhf()
+	buffer.WriteString(importstr)
+	//\n ) \n
+	buffer.WriteString(zfzhi.Zhi.Hhf() + zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf())
+}
+func fangfamoban(bianma string, buffer *bytes.Buffer) {
+
+}
 func Shengchengmoban() {
 	mkarr := Mokuaimingsarr
 	mks := Mokuaimings
@@ -16,11 +27,14 @@ func Shengchengmoban() {
 		for bk, _ := range biaos {
 			buffer := &bytes.Buffer{}
 			buffer.WriteString(mkv)
+			importsmoban(mkv, buffer)
+			fangfamoban(bk, buffer)
+
 			dir := Getgopath() + zfzhi.Zhi.Xx() + mkv +
 				zfzhi.Zhi.Xx() + zf.Zfs.Zd(true) + zf.Zfs.Fortests(true)
 			path := dir + zfzhi.Zhi.Xx() + bk + zf.Zfs.Fortests(true) + zfzhi.Zhi.Dh() + zf.Zfs.Go(true)
 			log.Println(buffer.String())
-			log.Println("path", path)
+			log.Println("path----------------", path)
 		}
 
 	}
