ed0d3208e81a7a712fd577898df4d4492d3e51ea
changlz <changlizhi@aliyun.com>
2017-10-18 23:30:42 +0800

    把表属性的方法放到一个文件

--- Biaoshuxing.go
+++ Biaoshuxing.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"reflect"
 	"strings"
+	"gongju/peizhi"
 )
 
 func Fanshebiaolies(mokuaiming string) (biaolie map[string]string, biao map[string]string, lie map[string]string) {
@@ -56,6 +57,43 @@ func Zhongwen(lieming string) string {
 	if ret == zfzhi.Zhi.Kzf() {
 		log.Println("类型错误-----空")
 		return zfzhi.Zhi.Kzf()
+	}
+	return ret
+}
+
+func Fanshejichulie() []string {
+	m := &Jicuziduan{}
+	ret := []string{}
+	rvm := reflect.ValueOf(m)
+	for i := zfzhi.Zhi.Shuzi0(); i < rvm.NumMethod(); i++ {
+		rvmv := rvm.Method(i).Call(nil)[zfzhi.Zhi.Shuzi0()].String()
+		ret = append(ret, rvmv)
+	}
+	return ret
+}
+
+func Fanshebiaojiegou(mokuaiming string) map[string][]string {
+	//循环所有Mokuaiming里的字段类型如果匹配了参数则成功
+	m := peizhi.Mokuaiming{}
+	rvm := reflect.ValueOf(m)
+	ret := make(map[string][]string)
+
+	for i := zfzhi.Zhi.Shuzi0(); i < rvm.NumField(); i++ {
+		rvmf := rvm.Field(i)
+		rvmftn := strings.ToLower(rvmf.Type().Name())
+		if rvmftn == mokuaiming {
+			for j := zfzhi.Zhi.Shuzi0(); j < rvmf.NumField(); j++ {
+				rvmff := rvmf.Field(j)
+				rvmffn := strings.Split(rvmff.Type().String(), zfzhi.Zhi.Dh())[zfzhi.Zhi.Shuzi1()]
+				vs := Fanshejichulie()
+				for k := zfzhi.Zhi.Shuzi0(); k < rvmff.NumMethod(); k++ {
+					rvmffm := rvmff.Method(k)
+					val := rvmffm.Call(nil)[zfzhi.Zhi.Shuzi0()].String()
+					vs = append(vs, val)
+				}
+				ret[rvmffn] = vs
+			}
+		}
 	}
 	return ret
 }
